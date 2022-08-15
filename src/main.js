29c234d91eb2a7f171ecf96abc3a44075b8348ff
IlyaChichkov <ilya555chich@yandex.ru>
2022-08-16 02:11:00 +0300

    lang switch done

--- src/main.js
+++ src/main.js
@@ -2,26 +2,7 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import './App.css'
 import VueI18n from "vue-i18n";
+import i18n from './i18n'
 
-const app = createApp(App).mount('#app')
+const app = createApp(App).use(i18n).mount('#app')
 app.use(VueI18n)
-
-const data = {
-    EN: {
-        profile: {
-            name: 'Ilya Chichkov'
-        }
-    },
-    RU: {
-        profile: {
-            name: 'Илья Чичков'
-        }
-    }
-}
-
-// Create VueI18n instance with options
-// eslint-disable-next-line no-unused-vars
-const localizeLib = new VueI18n({
-    locale: 'EN', // set locale
-    data, // set locale messages
-})
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/my-resume/'
+  publicPath: '/my-resume/',
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableLegacy: false,
+      runtimeOnly: false,
+      compositionOnly: false,
+      fullInstall: true
+    }
+  }
 })
