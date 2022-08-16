82205434fdddccaaaafa6623f39143ca3d2d271c
IlyaChichkov <ilya555chich@yandex.ru>
2022-08-16 10:28:06 +0300

    content change

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="dark">
     <section>
-      <div class="flex">
+      <div class="flex justify-evenly">
         <img class="avatar-icon" src="../assets/avatar.png">
         <div class="flex-col justify-between ml-6">
           <h1 class="font-bold mt-0">{{$t("profile.title")}}</h1>
           <p>{{$t('profile.desc')}}</p>
           <div class="flex">
-            <p class="mr-2 my-0 font-bold text-white">{{$t('profile.city')}}</p>
+            <p class="mr-2 my-0 font-bold text-white">{{$t('profile.city')}}:</p>
             <p class="mr-4 my-0">{{$t('profile.moscow')}}</p>
-            <p class="mr-2 my-0 font-bold text-white">{{$t('profile.age')}}</p>
+            <p class="mr-2 my-0 font-bold text-white">{{$t('profile.age')}}:</p>
             <p class="mr-2 my-0">19</p>
           </div>
         </div>
       </div>
     </section>
     <section>
-      <ul class="flex justify-around links">
+      <ul class="flex justify-around links my-10">
         <li><a href="https://github.com/IlyaChichkov">Github</a></li>
         <li><a href="https://t.me/Ilya_Chichkov">Telegram</a></li>
         <li><a href="https://vk.com/chichkov_ilya">VK</a></li>
@@ -61,9 +61,9 @@
     <section>
       <h2>{{$t('titles.lang')}}</h2>
       <div class="flex">
-        <ProgressCircle :title="'Russian'" :percent="100" :size="80"/>
-        <ProgressCircle :title="'English'" :percent="70" :size="80"/>
-        <ProgressCircle :title="'German'" :percent="10" :size="80"/>
+        <ProgressCircle :title="$t('languages.ru')" :percent="100" :size="80"/>
+        <ProgressCircle :title="$t('languages.en')" :percent="70" :size="80"/>
+        <ProgressCircle :title="$t('languages.ge')" :percent="10" :size="80"/>
       </div>
     </section>
     <section>
