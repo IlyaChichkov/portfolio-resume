862b4682352c069d1b27130801c48bae7d4caf08
IlyaChichkov <ilya555chich@yandex.ru>
2022-08-16 16:08:22 +0300

    content added

--- src/components/MyProjectsSection.vue
+++ src/components/MyProjectsSection.vue
@@ -1,8 +1,17 @@
 <template>
   <h2>{{$t('titles.proj')}}</h2>
-  <div class="border-solid border-0 border-l-4 border-l-red pl-4">
-    <p>{{$t('projects.one')}}</p>
-    <p>{{$t('projects.two')}}</p>
+  <div>
+    <ul class="border-solid border-0 border-l-1 border-l-red pl-0">
+      <li class="border-solid border-0 border-l-4 border-l-red pl-4">
+        <p>{{$t('projects.one')}}</p>
+        <img class="image" src="../assets/space-front.png"
+             alt="space front screenshot">
+      </li>
+      <li class="border-solid border-0 border-l-4 border-l-red pl-4">
+        <p>{{$t('projects.two')}}</p>
+        <img class="image" src="../assets/dawn-of-white.jpg">
+      </li>
+    </ul>
     <p>{{$t('projects.three')}}</p>
   </div>
 </template>
@@ -14,5 +23,7 @@ export default {
 </script>
 
 <style scoped>
-
+.image{
+  @apply outline outline-offset-2 outline-1 outline-red p-1 max-w-[94%] sm:max-w-[452px];
+}
 </style>
