7b1c3ba6a542e364361e00daa9dbb522ce4f7782
AenclaveGames <55506834+IlyaChichkov@users.noreply.github.com>
2023-01-11 18:45:29 +0300

    change: update dependencies

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/portfolio-resume/',
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/portfolio-resume/'
+      : '/',
 
   pluginOptions: {
     i18n: {
